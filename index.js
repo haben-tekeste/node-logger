330ca1f76cae7ccbdd51e868dd9f0832bcb8734c
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-09-06 08:11:31 +0000

    added logger class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const LogLevel = class  {
+class LogLevel  {
     static Debug = 0;
     static Info = 1;
     static Warn = 2;
@@ -7,7 +7,6 @@ export const LogLevel = class  {
 
     // verifies the log level is valid and supported
     static assert(log_level){
-        console.log(log_level === LogLevel.Info);
         if (![
             LogLevel.Debug, LogLevel.Info, LogLevel.Critical, LogLevel.Error, LogLevel.Warn
         ].includes(log_level)
@@ -20,4 +19,30 @@ export const LogLevel = class  {
 }
 
 
+class Logger {
+    // set default value of level to be LogLevel.Debug for 
+    // every new instance
+     #level = LogLevel.Debug;
+    
+    
+    constructor(log_level){
+        // set log level only when provided
+        // else will be set by default to LogLevel.Debug
+        if (arguments.length){
+            // throw error if log_level is Unsupported
+            LogLevel.assert(log_level)
+            this.#level = log_level;
+        }
+
+    }
+    
+    get level(){
+        return this.#level
+    }
+    }
+    
+const logger = new Logger(LogLevel.Info)
+console.log(logger.level)
+
+export {LogLevel, Logger}
 
