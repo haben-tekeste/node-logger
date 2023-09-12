dcbec2256aaea3c452f81fbf379a93c52effce6f
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-09-12 07:25:55 +0000

    added method for handling file used to log

--- test.js
+++ test.js
@@ -1,13 +1,4 @@
 import {Logger, LogConfig} from "./index.js"
 
 const logger = Logger.with_config(LogConfig.from_file("./config.json"))
-console.log(logger.file_prefix)
-console.log(logger.time_threshold)
-console.log(logger.size_threshold)
-console.log(logger.level)
-
-logger.debug('Hello debug');
-logger.info('Hello info');
-logger.warn('Hello warning');
-logger.error('Hello error');
-logger.critical('Hello critical');
+await logger.init()
