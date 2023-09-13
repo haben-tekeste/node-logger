7a4a0106f47b699d4e5e07a99565e9984a5dbc5c
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-09-13 11:30:58 +0000

    helper function to trace stack

--- test.js
+++ test.js
@@ -5,5 +5,22 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-const logger = Logger.with_config(LogConfig.from_file(path.join(__dirname,"./config.json")))
-await logger.init()
+async function initialize_logger(){
+    let logger = Logger.with_config(LogConfig.from_file(path.join(__dirname,"./config.json")))
+    await logger.init()
+    return logger
+}
+
+async function main() {
+    let logger = await initialize_logger()
+    logger.critical('From the main() function')
+    nested_func(logger)
+    }
+    function nested_func(logger) {
+    logger.critical('From the nested_func() function')
+    super_nested(logger)
+    }
+    function super_nested(logger) {
+    logger.critical('From the super_nested() function')
+    }
+    main()
